df3ab9fe061418d6cedf4be05bfd51ad0be30f11
FelisDiligens <47528453+FelisDiligens@users.noreply.github.com>
2022-11-23 18:49:14 +0100

    Fixing race condition

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,6 @@ joplin.plugins.register({
 		// Register this plugin's settings, so the user can access them:
 		await registerAllSettings();
 
-		// Register a CodeMirror plugin, so the user's preferences actually get applied:
-		await joplin.contentScripts.register(
-			ContentScriptType.CodeMirrorPlugin,
-			CONTENT_SCRIPT_ID,
-			'./cmoptions.js'
-		);
-
 		// Register a message, so our CodeMirror plugin can retrieve the user's preferences:
 		await joplin.contentScripts.onMessage(CONTENT_SCRIPT_ID, async (message: any) => {
 			if (message.name === 'getCMOptions') {
@@ -24,5 +17,12 @@ joplin.plugins.register({
 			}
 			return "Error: " + message + " is not a valid message";
 		});
+
+		// Register a CodeMirror plugin, so the user's preferences actually get applied:
+		await joplin.contentScripts.register(
+			ContentScriptType.CodeMirrorPlugin,
+			CONTENT_SCRIPT_ID,
+			'./cmoptions.js'
+		);
 	},
 });
