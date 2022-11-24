cc3db330583818d5d878d4f36a2c6ef10124a8f6
FelisDiligens <47528453+FelisDiligens@users.noreply.github.com>
2022-11-24 07:03:34 +0100

    Updating a comment, since my understanding grew

--- src/cmoptions.ts
+++ src/cmoptions.ts
@@ -6,8 +6,7 @@ module.exports = {
     default: function(context) { 
         return {
             plugin: function (CodeMirror) {
-                // Define a CM option, so we can get a (working) CodeMirror instance (cm) that we can manipulate:
-                // (For some reason CodeMirror.setOption() "is not a function")
+                // Define a CM option, so we can get a CodeMirror.Editor instance (cm) that we can manipulate:
                 CodeMirror.defineOption('enableMoreCMOptions', false, async function(cm, val, old) {
                     // Get all CM options that the user has set:
                     let cmoptions = await context.postMessage({ name: 'getCMOptions' });
